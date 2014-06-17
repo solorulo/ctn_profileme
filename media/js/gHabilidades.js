0b723c6504370d252d22a14c5221fb81b44664cb
solorulo <solorulo@hotmail.com>
2014-06-16 22:35:06 -0500

    Acomodo de la plantilla de perfil. Abstraccion de las tablas de habilidades y herramientas.

---
+++ media/js/gHabilidades.js
@@ -0,0 +1,71 @@
+var categoriasHabilidades = ['Pert,CPM', 'Business Intelligence', 'Gantt', 'Canvas Bussiness Model',
+	'Plan de negocios', 'Otra'
+];
+var dataHabilidades = [990, 4440, 660, 770, 1000, 5];
+
+function graficaHabilidades() {
+	$('#container').highcharts({
+		chart: {
+			polar: true,
+			backgroundColor: 'transparent',
+			type: 'line'
+
+		},
+		credits: {
+			enabled: false
+		},
+		title: {
+			text: 'Habilidades',
+			x: -80,
+			style: {
+				color: '#459fff',
+				fontWeight: 'Verdana, sans-serif'
+			}
+
+		},
+		pane: {
+			size: '80%'
+		},
+		xAxis: {
+			categories: categoriasHabilidades,
+			labels: {
+				align: 'right',
+				style: {
+					color: '#459fff',
+					fontSize: '11px',
+					fontFamily: 'Verdana, sans-serif'
+				}
+			},
+			tickmarkPlacement: 'on',
+			lineWidth: 0
+
+		},
+		yAxis: {
+			gridLineInterpolation: 'polygon',
+			lineWidth: 0,
+			min: 0
+		},
+		tooltip: {
+			shared: true,
+			pointFormat: '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}</b><br/>'
+		},
+		legend: {
+			enabled: false,
+			align: 'right',
+			verticalAlign: 'top',
+			y: 70,
+			layout: 'vertical'
+		},
+		exporting: {
+			enabled: false
+
+		},
+		series: [{
+			name: "Porcentaje",
+			data: dataHabilidades,
+			color: '#459fff',
+			pointPlacement: 'on',
+
+		}]
+	});
+}
---
+++ media/js/gHerramientas.js
@@ -0,0 +1,86 @@
+var categoriasHerramientas = [
+				'Java',
+				'JavaScript',
+				'CSS',
+				'HTML',
+				'C#',
+				'jQuery'
+			];
+var dataHerramientas = [100, 60, 50, 80, 10, 50];
+function tablaHerramientas() {
+	
+}
+function graficaHerramientas() {
+	$('#container2').highcharts({
+		chart: {
+			type: 'column',
+			backgroundColor: 'transparent',
+			margin: [60, 60, 100, 80],
+		},
+		credits: {
+			enabled: false
+		},
+		title: {
+			text: 'Herramientas',
+			style: {
+				marginleft: '0px',
+				color: '#ff738f',
+				fontWeight: 'Verdana, sans-serif'
+			}
+		},
+		xAxis: {
+			categories: categoriasHerramientas,
+			labels: {
+				rotation: -90,
+				align: 'right',
+				style: {
+					color: '#ff738f',
+					fontSize: '11px',
+					fontFamily: 'Verdana, sans-serif'
+
+				}
+			}
+		},
+		yAxis: {
+			min: 0,
+			title: {
+				style: {
+					color: '#ff738f',
+					fontWeight: 'Verdana, sans-serif'
+				}
+
+			}
+
+		},
+		legend: {
+			enabled: false
+		},
+		tooltip: {
+			formatter: function() {
+				return '<b>' + this.x + '</b><br/>' +
+					Highcharts.numberFormat(this.y);
+			}
+		},
+		exporting: {
+			enabled: false
+
+		},
+		series: [{
+			name: 'Population',
+			data: dataHerramientas,
+			color: '#459fff',
+			dataLabels: {
+				enabled: true,
+				rotation: -90,
+				color: 'white',
+				align: 'right',
+				x: 4,
+				y: 10,
+				style: {
+					fontSize: '10px',
+					fontFamily: 'Verdana, sans-serif'
+				}
+			}
+		}]
+	});
+}
