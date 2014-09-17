84957516b70f7dc6e1b75e4acd108ca737759e4e
Rulo <solorulo@hotmail.com>
2014-09-16 19:22:53 -0500

    Allowed host modificado

--- profileme/settings.py
+++ profileme/settings.py
@@ -53,7 +53,7 @@ else :
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.profileme.com.mx', 'profileme.com.mx', 'profileme.herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
